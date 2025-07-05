e7c346820ef0f58528d4b0093a6f77fff3a36b7d
pranay <pranay2632@gmail.com>
2025-07-05 22:53:42 +0530

    clearing errors bye .env

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose"
-import { DB_NAME } from "../constants"
+import { DB_NAME } from "../constants.js"
 const connectDB=async()=>{
     try{
-        const connect=await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        const connect=await mongoose.connect(`mongodb+srv://pranayenugurthi:pranaykumar2629@cluster0.ishvpui.mongodb.net/${DB_NAME}`)
         console.log("Host",`${connect.connection.host}`);
 
     }catch(error){
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-require("dotenv").config({path:"./env"})
-import connectDB from "./db"
+//require("dotenv").config({path:"./env"})
+import connectDB from "./db/index.js"
+//import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+// dotenv.config({
+//     path:".env"
+// })
 connectDB()
